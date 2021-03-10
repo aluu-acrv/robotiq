fa3f6e3ade85ac5c032cca1f3165de2cac675bf2
aluu <a.luu@qut.edu.au>
2021-03-10 16:18:57 +1000

    Adds force torque transformer
    parent and child frames set in the private variables

---
+++ robotiq_ft_sensor/src/ft_transformer.cpp
@@ -0,0 +1,129 @@
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/WrenchStamped.h>
+#include <ros/ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
+
+/**
+ * @author Baris Balic & Amelia Luu
+ * @date 06/09/19
+ * @version 1.1 - uses tf frames to tranform the force readings.
+ * @brief main: the force torque sensor is mounted to the ur10's last joint. this tf frame is transformed
+ * to where the linishing roller's tcp point is, such that the force readings are with respect to the
+ * roller's frame.
+ */
+
+class FT_Transformer
+{
+private:
+  ros::NodeHandle* m_node;
+  ros::Publisher ft_pub;
+  ros::Subscriber sub;
+
+  std::string ft_parent_frame = "tool0_controller";  // where the force torque readings originate from
+  std::string ft_child_frame = "fts_frame";          // where the ft sensor actually is located
+
+  geometry_msgs::TransformStamped transformStamped;
+
+public:
+  /**
+   * @brief Construct a new ft transformer object
+   *
+   * @param node
+   */
+  FT_Transformer(ros::NodeHandle* node);
+
+  /**
+   * @brief Destroy the ft transformer object
+   *
+   */
+  ~FT_Transformer();
+
+  /**
+   * @brief Initialises the ROS subscriber and publisher
+   *
+   */
+  void initialiseROS();
+
+  /**
+   * @brief Finds the transform from parent to child frame
+   *
+   * @param parent the original frame
+   * @param child the frame we want it in
+   * @return geometry_msgs::TransformStamped the transform between frames
+   */
+  geometry_msgs::TransformStamped getTransform(std::string parent, std::string child);
+
+  /**
+   * @brief The force torque data call back from original sensor.
+   *
+   * @param msg the input geometry_msgs::WrenchStamped message
+   */
+  void callback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
+};
+
+FT_Transformer::FT_Transformer(ros::NodeHandle* node) : m_node(node)
+{
+  ROS_INFO("Attempting to get the transform...");
+  transformStamped = getTransform(ft_parent_frame, ft_child_frame);
+
+  ROS_INFO("Got it! Initialising ROS...");
+  initialiseROS();
+
+  ROS_INFO("Done");
+}
+
+FT_Transformer::~FT_Transformer()
+{
+}
+
+void FT_Transformer::initialiseROS()
+{
+  ft_pub = m_node->advertise<geometry_msgs::WrenchStamped>("/ft_transformed", 1);
+  sub = m_node->subscribe("/robotiq_ft_wrench", 1, &FT_Transformer::callback, this);
+}
+
+geometry_msgs::TransformStamped FT_Transformer::getTransform(std::string parent, std::string child)
+{
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+  geometry_msgs::TransformStamped t;
+  bool success;
+  while (!success)
+  {
+    try
+    {
+      t = tfBuffer.lookupTransform(parent, child, ros::Time(0));
+      ROS_INFO_STREAM("The transform from " << parent << " to " << child << " is: \n" << t);
+      return t;
+    }
+
+    catch (tf2::TransformException& ex)
+    {
+      ROS_WARN_THROTTLE(1, "%s", ex.what());
+      ros::Duration(0.5).sleep();
+    }
+  }
+}
+
+void FT_Transformer::callback(const geometry_msgs::WrenchStamped::ConstPtr& msg)
+{
+  // do the transform and publish it
+  ROS_INFO_STREAM_ONCE("Got first geometry_msgs::WrenchStamped message! \n" << *msg);
+  geometry_msgs::WrenchStamped transformed_ft_msg;
+  tf2::doTransform(*msg, transformed_ft_msg, transformStamped);
+  ROS_INFO_STREAM_ONCE("Transformed it to: \n" << transformed_ft_msg);
+  ft_pub.publish(transformed_ft_msg);
+  ROS_INFO_STREAM_ONCE("Published transformed data");
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "force_torque_transformer");
+
+  ros::NodeHandle nh;
+  FT_Transformer ftt(&nh);
+
+  ros::spin();
+  return 0;
+};
